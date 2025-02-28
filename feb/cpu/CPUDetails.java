776c20beb57cc4cc73c69b913c83e35aa6fa63f7
rosh4976 <roshnashirinm2002@gmail.com>
2025-02-28 15:39:57 +0530

    Delete feb/cpu directory

--- feb/cpu/CPUDetails.java
+++
@@ -1,9 +0,0 @@
-public class CPUDetails{
-	public static void main(String[] args) {
-		CPU cpu=new CPU();
-		CPU.processor processor=cpu.new processor();
-		CPU.RAM ram=new CPU.RAM();
-		System.out.println("processor Cache="+processor.getCache());
-		System.out.println("RAM Clock speed="+ram.getClockSpeed());
-	}
-}
