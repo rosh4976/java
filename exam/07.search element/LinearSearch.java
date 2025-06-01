852c23dd6d2535648dd5033ff4753b5fda382aaf
rosh4976 <roshnashirinm2002@gmail.com>
2025-06-01 16:57:38 +0530

    Update LinearSearch.java

--- exam/07.search element/LinearSearch.java
+++ exam/07.search element/LinearSearch.java
@@ -7,7 +7,7 @@ public class LinearSearch {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter the number of elements in the array:");
 		int count=sc.nextInt();
-		sc.nextLine();
+		
 		
 		int array[]=new int[count];
 		
@@ -15,24 +15,28 @@ public class LinearSearch {
 			System.out.println("enter the "+(i+1)+"element");
 			array[i]=sc.nextInt();}
 		
-			sc.nextLine();
+		
 	
 		
 		System.out.println("enter the element to search in the array:");
 		int search=sc.nextInt();
-		for(int i=0;i<count;i++) {
+		int i;
+		for(i=0;i<count;i++) {
 			  if(array[i]==search) {
 				System.out.println("the element is present in array position"+(i+1));
 				break;
 			  }	
+		}
+		if(i==count) {
+			
 		
 		
 				System.out.println("the element is not present in the array");
 				
-			
 		}
+	}}
 		
 		
 		
 
-	}}
+	
