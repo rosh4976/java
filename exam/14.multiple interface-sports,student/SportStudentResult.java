978af15cf65415706dbfde4100bfa155854adc70
rosh4976 <roshnashirinm2002@gmail.com>
2025-06-02 23:10:14 +0530

    Update SportStudentResult.java

--- exam/14.multiple interface-sports,student/SportStudentResult.java
+++ exam/14.multiple interface-sports,student/SportStudentResult.java
@@ -14,10 +14,12 @@ interface Sports
 
 class Result implements Student,Sports
 {
+	@Override
 	public void displayScore()
 	{
 		System.out.println("Academic score is:"+Student.score);
 	}
+	@Override
 	public void displaySportsScore()
 	{
 		System.out.println("Sports Score is:"+Sports.score);
