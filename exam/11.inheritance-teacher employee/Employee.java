c4c7c7be8b76f3ec8af7e27e5a69edf7f23dd26a
rosh4976 <roshnashirinm2002@gmail.com>
2025-06-02 13:33:08 +0530

    Create Employee.java

---
+++ exam/11.inheritance-teacher employee/Employee.java
@@ -0,0 +1,17 @@
+package EXAM;
+
+public class EmployeeT {
+
+    int empid;
+	String name,address;
+	float salary; 
+	public EmployeeT() {}
+	
+	public EmployeeT(int empid,String name,float salary,String address) 
+	{
+		this.empid=empid;
+		this.name=name;
+		this.salary=salary;
+		this.address=address;
+	}
+}
