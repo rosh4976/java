59f59a679a4a9ebd09f19c89edca7c903b1723cb
rosh4976 <roshnashirinm2002@gmail.com>
2025-06-02 23:08:16 +0530

    Create SportStudentResult.java

---
+++ exam/14.multiple interface-sports,student/SportStudentResult.java
@@ -0,0 +1,35 @@
+package EXAM;
+
+interface Student
+{
+	int score = 5;
+	void displayScore();
+}
+
+interface Sports
+{
+	int score = 25;
+	void displaySportsScore();
+}
+
+class Result implements Student,Sports
+{
+	public void displayScore()
+	{
+		System.out.println("Academic score is:"+Student.score);
+	}
+	public void displaySportsScore()
+	{
+		System.out.println("Sports Score is:"+Sports.score);
+	}
+}
+
+public class SportStudentResult {
+
+	public static void main(String[] args) {
+		
+		Result r = new Result();
+		r.displayScore();
+		r.displaySportsScore();
+	}
+}
